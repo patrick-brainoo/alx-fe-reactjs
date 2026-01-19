d84c3ab3441cfefd79b9630bacd34274dfa14044
Patrick Brainoo <patrickbrainoo@gmail.com>
2026-01-18 21:42:16 -0800

    Add components and apply inline styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#f2f2f2', textAlign: 'center', padding: '10px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
