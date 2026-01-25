7425e28a8194856c37af1090252dff9794b6365c
Patrick Brainoo <patrickbrainoo@gmail.com>
2026-01-24 23:43:34 -0800

    Made changes to Store

--- alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#f2f2f2', textAlign: 'center', padding: '10px' }}>
-      <p>© 2023 City Lovers</p>
-    </footer>
-  );
-}
-
-export default Footer;
