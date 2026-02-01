1e3b20e905582974ec0f4db276f1c8bb1f84f804
Patrick Brainoo <patrickbrainoo@gmail.com>
2026-02-01 12:53:14 -0800

    Add components directory and import component in App

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,8 +1,10 @@
+import SearchBar from "./components/SearchBar";
+
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <p>Search for GitHub users</p>
+      <SearchBar />
     </div>
   );
 }
---
+++ github-user-search/src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" placeholder="Search GitHub users..." />
+    </div>
+  );
+}
+
+export default SearchBar;
