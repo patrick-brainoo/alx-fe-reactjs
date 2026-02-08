947ba2d1cafff8c2a26021807427fe6802609e3f
Patrick Brainoo <patrickbrainoo@gmail.com>
2026-02-07 22:51:23 -0800

    Apply responsive container, typography and image sizing with Tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,9 +3,12 @@ function UserProfile() {
     <div
       className="
         bg-gray-100
-        p-4 md:p-8
-        max-w-xs md:max-w-sm
-        mx-auto my-20
+        sm:p-4
+        md:p-8
+        sm:max-w-xs
+        md:max-w-sm
+        mx-auto
+        my-20
         rounded-lg
         shadow-lg
         text-center
@@ -17,14 +20,15 @@ function UserProfile() {
         className="
           rounded-full
           mx-auto
-          w-24 h-24
+          sm:w-24 sm:h-24
           md:w-36 md:h-36
         "
       />
 
       <h1
         className="
-          text-lg md:text-xl
+          sm:text-lg
+          md:text-xl
           text-blue-800
           my-4
         "
@@ -35,7 +39,8 @@ function UserProfile() {
       <p
         className="
           text-gray-600
-          text-sm md:text-base
+          sm:text-sm
+          md:text-base
         "
       >
         Developer at Example Co. Loves to write code and explore new technologies.
