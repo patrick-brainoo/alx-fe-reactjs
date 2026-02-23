433c256d11ef26ff198159ffcb6b8aafa0c94a90
Patrick Brainoo <patrickbrainoo@gmail.com>
2026-02-22 22:02:31 -0800

    Made changes to main and app files

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import PostsComponent from "./components/PostsComponent";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div style={{ padding: "2rem" }}>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
